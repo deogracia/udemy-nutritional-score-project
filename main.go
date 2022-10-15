483bb9f5476ca898478a1098792d95d57deaa63f
Lionel Félicité <deogracia@free.fr>
2022-10-15 14:08:18 +0400

    initial implementation

--- main.go
+++ main.go
@@ -1,17 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
+
+	help := flag.Bool("help", false, "Show help")
+	energy := flag.Float64("energy", -42, "Energy (Kcal)")
+	sugars := flag.Float64("sugars", -42, "Sugars (g)")
+	sfa := flag.Float64("sfa", -42, "Saturated Fatty Acids (g)")
+	sodium := flag.Float64("sodium", -42, "Sodium (g)")
+	fiber := flag.Float64("fiber", -42, "Fiber (g)")
+	protein := flag.Float64("protein", -42, "Protein (g)")
+	fruitspercent := flag.Float64("fruitspercent", -42, "Fruits Percent")
+
+	flag.Parse()
+
+	if *help {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
 	fmt.Println("vim-go")
 	ns := GetNutritionalScore(NutritionalData{
-		Energy:              EnergyFromKcal(100),
-		Sugars:              SugarGram(10),
-		SaturatedFattyAcids: SaturatedFattyAcids(2),
-		Sodium:              SodiumMilligram(500),
-		Fruits:              FruitsPercent(60),
-		Fiber:               FiberGram(4),
-		Protein:             ProteinGram(2),
+		Energy:              EnergyFromKcal(*energy),
+		Sugars:              SugarGram(*sugars),
+		SaturatedFattyAcids: SaturatedFattyAcids(*sfa),
+		Sodium:              SodiumMilligram(*sodium),
+		Fruits:              FruitsPercent(*fruitspercent),
+		Fiber:               FiberGram(*fiber),
+		Protein:             ProteinGram(*protein),
 	}, Food)
 
 	fmt.Printf("Nutritional Score is %v\n", ns.Value)
